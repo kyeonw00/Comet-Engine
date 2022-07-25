f673295f032b5268c77821b30110a32307f5437d
kyeonw00 <altair3873.luna@gmail.com>
2022-07-25 23:36:15 +0900

    additional pre-process added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define GLFW_INCLUDE_NONE
+#include <gl/GL.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -6,10 +8,14 @@ int main(void)
 
     /* Initialize the library */
     if (!glfwInit())
+    {
         return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    window = glfwCreateWindow(640, 480, "Comet Engine", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -22,9 +28,6 @@ int main(void)
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
